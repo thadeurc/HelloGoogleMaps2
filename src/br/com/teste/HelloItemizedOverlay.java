f7bb17a92617f6e6eaa27f650bc2a7812a621ef2
Thadeu <thadeu.russo@caelum.com.br>
2011-04-13 23:43:45 -0300

    first commit

---
+++ src/br/com/teste/HelloItemizedOverlay.java
@@ -0,0 +1,46 @@
+package br.com.teste;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class HelloItemizedOverlay extends ItemizedOverlay<OverlayItem> {
+
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context context;
+	
+	public HelloItemizedOverlay(Drawable defaultMarker, Context context) {
+		super(boundCenterBottom(defaultMarker));
+		this.context = context;
+	}
+
+	@Override
+	protected boolean onTap(int index) {
+	  OverlayItem item = mOverlays.get(index);
+	  AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+	  dialog.setTitle(item.getTitle());
+	  dialog.setMessage(item.getSnippet());
+	  dialog.show();
+	  return true;
+	}
+	
+	@Override
+	protected OverlayItem createItem(int i) {
+		return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mOverlays.size();
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+}
